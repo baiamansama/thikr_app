8ce0899493ceba5f55d26d2ed212d6cff16f3bd5
Baiaman <baiamansama@gmail.com>
2025-02-28 14:11:48 +0900

    client: fix preview

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,9 +18,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: "зикир & дуба",
     description: "Мусулманга зарыл болгон зикирлер жана дубалар топтому",
+    url: "https://azkar.link",
     images: [
       {
-        url: "/preview/english.png",
+        url: "https://azkar.link/preview/english.png",
         width: 1200,
         height: 630,
         alt: "Thikr App Preview",
